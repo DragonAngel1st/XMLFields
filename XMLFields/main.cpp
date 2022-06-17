46ca4fb1e1f72a7955242ef23bd55be2f04d06bd
DragonAngel1st <31282296+DragonAngel1st@users.noreply.github.com>
2022-06-17 10:55:28 -0400

    Initial Commit

---
+++ XMLFields/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  XMLFields
+//
+//  Created by Patrick Miron on 2022-06-17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
